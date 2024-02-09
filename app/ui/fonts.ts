32e625e0446b18a188e53b3079bad75e4e4e7c31
James <james@realja.me>
2024-02-08 21:27:23 -0500

    end of chapter 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// International
+export const inter = Inter({ subsets: ['latin'] });
+
+// Lusitana
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
